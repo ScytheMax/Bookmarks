f4afe7f4f8c7acb76722fe50de77e3b645007eff
ScytheMax <maximilian.sari@t-online.de>
2020-12-12 11:32:30 +0100

    BMS-1: create win form project

---
+++ Bookmarks/Bookmarks/frmBookmarks.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ms.Bookmarks
+{
+	partial class frmBookmarks
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvBMS = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dgvBMS)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvBMS
+			// 
+			this.dgvBMS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvBMS.Location = new System.Drawing.Point(189, 130);
+			this.dgvBMS.Name = "dgvBMS";
+			this.dgvBMS.Size = new System.Drawing.Size(240, 150);
+			this.dgvBMS.TabIndex = 0;
+			// 
+			// frmBookmarks
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dgvBMS);
+			this.Name = "frmBookmarks";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dgvBMS)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgvBMS;
+	}
+}
+
